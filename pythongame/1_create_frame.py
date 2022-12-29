4e873f6525364730e9dd1c118f71aeb41ff851a3
지현 <372491@naver.com>
2022-12-30 00:05:52 +0900

    pygame 파이썬 게임 만들기

---
+++ pythongame/1_create_frame.py
@@ -0,0 +1,11 @@
+import pygame
+
+pygame.init() #초기화
+
+#화면 크기 설정
+screen_width = 480 #가로크기
+screen_height = 640 #세로크기
+screen = pygame.display.set_mode((screen_width, screen_height))
+
+#화면 타이틀 설정
+pygame.display.set_caption("Hi game")
