1246789f32967b482961666ac3a3cdeb0665de5f
지현 <372491@naver.com>
2023-02-06 23:16:30 +0900

    클래스에 리턴 값이 없는 메소드 추가, 객체 추가해서 값 넣기

--- NewClass.py
+++ NewClass.py
@@ -7,3 +7,19 @@ class Human:
 
 chi = Human("치이카와", 1, "??")
 print(chi.name)
+
+class Stock:
+    def __init__(self, name, number, updown) :
+        self.name = name
+        self.number = number
+        self.updown = updown
+    pass
+
+    def code(self, code) :
+        self.code = code
+    pass
+
+stock = Stock("samsung", 1000, "up")
+stock.code ("00590")
+print(stock.name, stock.number, stock.updown)
+print(stock.code)
